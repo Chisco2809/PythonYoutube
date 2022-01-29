3d44b8cc39ff5d574c1add37149d47699ef7590c
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-28 21:39:56 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ from flask import request
 
 app=flask.Flask(__name__)
 
+
+import youtube_dl
 @app.route('/',methods=['GET'])
 def home():
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
     videoid=request.args['VideoID']
-    return videoid
+    with ydl:
+        result = ydl.extract_info('https://www.youtube.com/watch?v='+videoid,download=False   )
+
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+        else:
+    # Just a video
+            video = result
+    
+    video_url = result['formats'][0]['url']
+
+    return video_url
