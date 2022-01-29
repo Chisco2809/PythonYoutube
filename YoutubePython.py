57659e08ddaee17786814c05364e3e6d9f8869d4
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-28 21:01:31 -0600

    Add files via upload

---
+++ YoutubePython.py
@@ -0,0 +1,19 @@
+import youtube_dl
+
+#ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+
+#with ydl:
+#    result = ydl.extract_info(
+#        'https://www.youtube.com/watch?v=-lLvtydTM78',
+#        download=False # We just want to extract the info
+#    )
+
+#if 'entries' in result:
+    # Can be a playlist or a list of videos
+#    video = result['entries'][0]
+#else:
+    # Just a video
+ #   video = result
+
+#video_url = result['formats'][0]['url']
+video_url='This is the output'
