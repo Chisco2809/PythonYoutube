ca95cf7a9e099e220c5f621d561fb6ffaef74c9f
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-30 21:47:19 -0600

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,25 @@ import youtube_dl
 @app.route('/',methods=['GET'])
 def home():
     
-    r = requests.get("http://unoredradio.com:9768/;stream.mp3_", stream=True)
+    
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
+    videoid=request.args['VideoID']
+    with ydl:
+        result = ydl.extract_info('https://www.youtube.com/watch?v='+videoid,download=False   )
+
+        if 'entries' in result:
+            # Can be a playlist or a list of videos
+            video = result['entries'][0]
+        else:
+    # Just a video
+            video = result
+    
+    video_url = result['formats'][0]['url']
+
+
+    
+    
+    r = requests.get(video_url, stream=True)
     return Response(stream_with_context(r.iter_content()),                 content_type = r.headers['Content-Type'])
     
 
