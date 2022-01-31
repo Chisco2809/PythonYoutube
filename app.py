f4b0bc3da89f1f27dbf3bc96d3a090193081489a
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-30 19:41:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -13,19 +13,4 @@ app=flask.Flask(__name__)
 import youtube_dl
 @app.route('/',methods=['GET'])
 def home():
-    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s.%(ext)s'})
-    videoid=request.args['VideoID']
-    with ydl:
-        result = ydl.extract_info('https://www.youtube.com/watch?v='+videoid,download=False   )
-
-        if 'entries' in result:
-            # Can be a playlist or a list of videos
-            video = result['entries'][0]
-        else:
-    # Just a video
-            video = result
-    
-    video_url = result['formats'][0]['url']
-
- 
-    return redirect(video_url)
+    return redirect("http://www.example.com", code=302)
