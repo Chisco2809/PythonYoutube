9bdeef4a4b776c85be1d2bb228040afc74ca63a5
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-30 21:23:02 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ Created on Fri Jan 28 21:10:13 2022
 @author: Usuario
 """
 import flask
-from flask import request,redirect
-import webbrowser
-
+from flask import request,redirect,Response
+import requests
 app=flask.Flask(__name__)
 
 
@@ -15,5 +14,7 @@ import youtube_dl
 @app.route('/',methods=['GET'])
 def home():
     r = requests.get("http://unoredradio.com:9768/;stream.mp3_", stream=True)
+    
+    
     return Response(r.iter_content(chunk_size=1024))
 
