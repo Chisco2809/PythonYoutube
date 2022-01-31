90ead1e4a935f41ff977cb34dc653a7736a09c34
Chisco2809 <98629248+Chisco2809@users.noreply.github.com>
2022-01-30 21:44:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ Created on Fri Jan 28 21:10:13 2022
 @author: Usuario
 """
 import flask
-from flask import request,redirect,Response
+from flask import request,redirect,Response,stream_with_context
 import requests
 app=flask.Flask(__name__)
 
@@ -13,7 +13,8 @@ app=flask.Flask(__name__)
 import youtube_dl
 @app.route('/',methods=['GET'])
 def home():
+    
     r = requests.get("http://unoredradio.com:9768/;stream.mp3_", stream=True)
-    return Response(stream_with_context(result.iter_content()),                 content_type = result.headers['Content-Type'])
+    return Response(stream_with_context(r.iter_content()),                 content_type = r.headers['Content-Type'])
     
 
